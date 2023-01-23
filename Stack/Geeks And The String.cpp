23ef7c388aff81ab99e7788de338e54a8c1f2224
Aashay Kaurav <112794592+aashayKaurav@users.noreply.github.com>
2023-01-23 23:38:52 +0530

    Update Geeks And The String.cpp

--- Stack/Geeks And The String.cpp
+++ Stack/Geeks And The String.cpp
@@ -1,3 +1,10 @@
+Our geek loves to play with strings, Currently, he is trying to reduce the size of a string by recursively removing all the consecutive duplicate pairs. In other words, He can apply the below operations any number of times.
+
+Remove all the consecutive duplicate pairs and concatenate the remaining string to replace the original string.
+Your task is to find the string with minimum length after applying the above operations.
+
+Note: If the string length become zero after applying operations, return "-1" as a string.
+
 string removePair(string s) {
         if(s.size() == 1)
             return s;
